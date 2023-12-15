b453e0eb5240f864d5488d43d7606fa7efbdc1b3
Corey Bertelsen <corey.bertelsen@avalanchestudios.se>
2023-12-15 14:18:33 +0100

    day 7 init

---
+++ Day7/Day7.cpp
@@ -0,0 +1,14 @@
+// Day7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void partOne()
+{
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
